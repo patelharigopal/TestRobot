5d5b150887739f1e5e240445a7d5633c2e6efe8a
Harigopal Patel <patel.harigopal@gmail.com>
2018-04-23 23:12:31 +0530

    Login Module

---
+++ README.md
@@ -0,0 +1,10 @@
+# TestRobot
+
+1. mvn clean
+2. mvn clean install
+3. Go to the target folder
+4. java -jar TestRobot-1.0.0-SNAPSHOT.jar
+
+- http://localhost:8080/registration
+- http://localhost:8080/login
+
---
+++ src/main/java/com/robot/service/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.robot.service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.robot.model.Role;
+import com.robot.model.User;
+import com.robot.repository.RoleRepository;
+import com.robot.repository.UserRepository;
+
+@Service("userService")
+public class UserServiceImpl implements UserService{
+
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Override
+	public User findUserByEmail(String email) {
+		return userRepository.findByEmail(email);
+	}
+
+	@Override
+	public void saveUser(User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(1);
+        Role userRole = roleRepository.findByRole("ADMIN");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+		userRepository.save(user);
+	}
+
+}
